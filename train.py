2424333574d83cc7f4e09d932f4be83cb27523b8
ryanrahmadifa <89628288+ryanrahmadifa@users.noreply.github.com>
2023-12-24 10:16:15 +0700

    DCGAN update

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 """
-Training of WGAN-GP
+Training of DCGAN on MNIST and CELEB
+"""
 
-Programmed by Aladdin Persson <aladdin.persson at hotmail dot com>
-* 2020-11-01: Initial coding
-* 2022-12-20: Small revision of code, checked that it works with latest PyTorch version
+"""
+Training of DCGAN network on MNIST dataset with Discriminator
+and Generator imported from models.py
 """
 
 import torch
@@ -14,95 +15,77 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
-from utils import gradient_penalty, save_checkpoint, load_checkpoint
 from model import Discriminator, Generator, initialize_weights
 
 # Hyperparameters etc.
-device = "cuda" if torch.cuda.is_available() else "cpu"
-LEARNING_RATE = 1e-4
-BATCH_SIZE = 64
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 128
 IMAGE_SIZE = 64
-CHANNELS_IMG = 3
-Z_DIM = 100
-NUM_EPOCHS = 100
-FEATURES_CRITIC = 16
-FEATURES_GEN = 16
-CRITIC_ITERATIONS = 5
-LAMBDA_GP = 10
+CHANNELS_IMG = 1
+NOISE_DIM = 100
+NUM_EPOCHS = 5
+FEATURES_DISC = 64
+FEATURES_GEN = 64
 
-transforms = transforms.Compose(
+transform = transforms.Compose(
     [
-        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.Resize(IMAGE_SIZE),
         transforms.ToTensor(),
         transforms.Normalize(
-            [0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]
+            [0.5 for _ in range(CHANNELS_IMG)],[0.5 for _ in range(CHANNELS_IMG)]
         ),
     ]
 )
 
-# dataset = datasets.MNIST(root="dataset/", transform=transforms, download=True)
-# comment mnist above and uncomment below for training on CelebA
-dataset = datasets.ImageFolder(root="dataset/celeb_dataset", transform=transforms)
-loader = DataLoader(
-    dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-)
+dataset = datasets.MNIST(root="dataset/", train=True, transform=transform, download=False)
 
-# initialize gen and disc, note: discriminator should be called critic,
-# according to WGAN paper (since it no longer outputs between [0, 1])
-gen = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
-critic = Discriminator(CHANNELS_IMG, FEATURES_CRITIC).to(device)
+loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+gen = Generator(NOISE_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+disc = Discriminator(CHANNELS_IMG, FEATURES_DISC).to(device)
 initialize_weights(gen)
-initialize_weights(critic)
+initialize_weights(disc)
 
-# initializate optimizer
-opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
-opt_critic = optim.Adam(critic.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+opt_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+criterion = nn.BCELoss()
 
-# for tensorboard plotting
-fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device)
-writer_real = SummaryWriter(f"logs/GAN_MNIST/real")
-writer_fake = SummaryWriter(f"logs/GAN_MNIST/fake")
+fixed_noise = torch.randn(32, NOISE_DIM, 1, 1).to(device)
+writer_real = SummaryWriter(f"logs/real")
+writer_fake = SummaryWriter(f"logs/fake")
 step = 0
 
 gen.train()
-critic.train()
+disc.train()
 
 for epoch in range(NUM_EPOCHS):
-    # Target labels not needed! <3 unsupervised
-    for batch_idx, (real, _) in enumerate(tqdm(loader)):
+    for batch_idx, (real, _) in enumerate(loader):
         real = real.to(device)
-        cur_batch_size = real.shape[0]
+        noise = torch.randn((BATCH_SIZE, NOISE_DIM, 1, 1)).to(device)
+        fake = gen(noise)
 
-        # Train Critic: max E[critic(real)] - E[critic(fake)]
-        # equivalent to minimizing the negative of that
-        for _ in range(CRITIC_ITERATIONS):
-            noise = torch.randn(cur_batch_size, Z_DIM, 1, 1).to(device)
-            fake = gen(noise)
-            critic_real = critic(real).reshape(-1)
-            critic_fake = critic(fake).reshape(-1)
-            gp = gradient_penalty(critic, real, fake, device=device)
-            loss_critic = (
-                -(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp
-            )
-            critic.zero_grad()
-            loss_critic.backward(retain_graph=True)
-            opt_critic.step()
+        ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
+        disc_real = disc(real).reshape(-1)
+        loss_disc_real = criterion(disc_real, torch.ones_like(disc_real))
+        disc_fake = disc(fake).reshape(-1)
+        loss_disc_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+        disc.zero_grad()
+        loss_disc.backward(retain_graph=True)
+        opt_disc.step()
 
-        # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
-        gen_fake = critic(fake).reshape(-1)
-        loss_gen = -torch.mean(gen_fake)
+        ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
+        output = disc(fake).reshape(-1)
+        loss_gen = criterion(output, torch.ones_like(output))
         gen.zero_grad()
         loss_gen.backward()
         opt_gen.step()
 
         # Print losses occasionally and print to tensorboard
-        if batch_idx % 100 == 0 and batch_idx > 0:
+        if batch_idx % 100 == 0:
             print(
                 f"Epoch [{epoch}/{NUM_EPOCHS}] Batch {batch_idx}/{len(loader)} \
-                  Loss D: {loss_critic:.4f}, loss G: {loss_gen:.4f}"
+                        Loss D: {loss_disc:.4f}, loss G: {loss_gen:.4f}"
             )
 
             with torch.no_grad():
